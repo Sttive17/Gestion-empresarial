6f5fcd2f768d280440e1a0587f8a4f5e99dcd6e4
Sttive17 <sttive@student.com>
2026-09-11 15:42:22 -0500

    Clase 5: Migraciones y modelos para el ERP - Relaciones y compatibilidad

--- sge/app/Models/Category.php
+++ sge/app/Models/Category.php
@@ -6,7 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['nombre', 'descripcion'];
+    protected $fillable = ['nombre', 'descripcion', 'name', 'description', 'active'];
+
+    public function getNameAttribute()
+    {
+        return $this->nombre;
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['nombre'] = $value;
+    }
+
+    public function getDescriptionAttribute()
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescriptionAttribute($value)
+    {
+        $this->attributes['descripcion'] = $value;
+    }
 
     public function products()
     {
--- sge/app/Models/Product.php
+++ sge/app/Models/Product.php
@@ -8,8 +8,42 @@ class Product extends Model
 {
     protected $fillable = [
         'category_id', 'codigo', 'nombre', 'descripcion', 
-        'precio', 'stock', 'estado'
+        'precio', 'stock', 'estado',
+        'name', 'description', 'price', 'active'
     ];
+
+    public function getNameAttribute()
+    {
+        return $this->nombre;
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['nombre'] = $value;
+        if (empty($this->attributes['codigo'])) {
+            $this->attributes['codigo'] = 'PROD-' . str_pad((static::max('id') ?? 0) + 1, 3, '0', STR_PAD_LEFT);
+        }
+    }
+
+    public function getDescriptionAttribute()
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescriptionAttribute($value)
+    {
+        $this->attributes['descripcion'] = $value;
+    }
+
+    public function getPriceAttribute()
+    {
+        return $this->precio;
+    }
+
+    public function setPriceAttribute($value)
+    {
+        $this->attributes['precio'] = $value;
+    }
 
     public function category()
     {
